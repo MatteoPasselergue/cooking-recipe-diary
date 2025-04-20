b7745f67889048c7592aa9f75275bafe3a069711
Matteo <passematteo@gmail.com>
2025-04-20 22:02:01 +0200

    Add ProfileSelection Screen
    - Added ProfileSelection Screen (include AddProfilDialog widget)
    - Edit main to include the profile selection on the first launch
    - Add lang values

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,22 @@ import 'package:cooking_recipe_diary/providers/CategoryProvider.dart';
 import 'package:cooking_recipe_diary/providers/LanguageProvider.dart';
 import 'package:cooking_recipe_diary/providers/RecipeProvider.dart';
 import 'package:cooking_recipe_diary/providers/TagProvider.dart';
+import 'package:cooking_recipe_diary/providers/UserProvider.dart';
 import 'package:cooking_recipe_diary/screens/HomeScreen.dart';
+import 'package:cooking_recipe_diary/screens/ProfileSelectionScreen.dart';
 import 'package:cooking_recipe_diary/services/LocalizationService.dart';
 import 'package:cooking_recipe_diary/utils/AppConfig.dart';
 import 'package:cooking_recipe_diary/utils/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  final profileData = prefs.getString('profile');
+
   await LocalizationService.load('fr_FR');
   await AppConfig.loadConfig();
   runApp(
@@ -19,15 +26,18 @@ void main() async {
         ChangeNotifierProvider(create: (_) => RecipeProvider()),
         ChangeNotifierProvider(create: (_) => CategoryProvider()),
         ChangeNotifierProvider(create: (_) => TagProvider()),
+        ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => LanguageProvider()),
       ],
-      child: const MyApp(),
+      child: MyApp(profileData: profileData),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final String? profileData;
+
+  const MyApp({Key? key, this.profileData}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +45,8 @@ class MyApp extends StatelessWidget {
       title: AppConfig.appName,
       theme: AppTheme.themeData,
       debugShowCheckedModeBanner: false,
-      home: const HomeScreen(),
+      home: profileData == null ? ProfileSelectionScreen() : const HomeScreen(),
+      //home: ProfileSelectionScreen(),
     );
   }
 }
---
+++ lib/screens/ProfileSelectionScreen.dart
@@ -0,0 +1,176 @@
+import 'dart:io';
+
+import 'package:cooking_recipe_diary/utils/AppConfig.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/UserModel.dart';
+import '../providers/UserProvider.dart';
+import '../services/ImageService.dart';
+import '../services/LocalizationService.dart';
+import '../widgets/dialogs/AddProfilDialog.dart';
+import 'HomeScreen.dart';
+
+class ProfileSelectionScreen extends StatefulWidget {
+  @override
+  _ProfileSelectionScreenState createState() => _ProfileSelectionScreenState();
+}
+
+class _ProfileSelectionScreenState extends State<ProfileSelectionScreen> {
+
+  FixedExtentScrollController _controller = FixedExtentScrollController();
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.microtask(() async {
+      try {
+        final userProvider = Provider.of<UserProvider>(context, listen: false);
+        await userProvider.fetchUsers();
+        await userProvider.loadProfile();
+
+        if (userProvider.profile != null) {
+          Navigator.of(context).popUntil((route) => route.isFirst);
+          Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const HomeScreen()));
+        }
+
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    });
+  }
+
+  bool _isLoading = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+
+    if (_isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return Scaffold(
+      backgroundColor: AppConfig.backgroundColor,
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.all(30)),
+          Text(
+            LocalizationService.translate("choose_profile"),
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: ListWheelScrollView.useDelegate(
+                controller: _controller,
+                itemExtent: 200, // hauteur de chaque item
+                physics: FixedExtentScrollPhysics(),
+                onSelectedItemChanged: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                perspective: 0.000001,
+                diameterRatio: 150,
+                childDelegate: ListWheelChildBuilderDelegate(
+                  builder: (context, index) {
+                    if (index < 0 || index >= userProvider.users.length+1) return null;
+
+                    bool isSelected = index == _selectedIndex;
+                    double size = isSelected ? 120 : 100;
+                    double fontSize = isSelected ? 25 : 21;
+
+                    if (index == 0) {
+                      return GestureDetector(
+                        onTap: isSelected ?() => _onAddProfile(userProvider) : null,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircleAvatar(
+                              radius: size / 2,
+                              backgroundColor: AppConfig.primaryColor,
+                              child: Icon(Icons.add, size: size / 2, color: AppConfig.backgroundColor,),
+                            ),
+                            Padding(padding: EdgeInsets.all(4)),
+                            Text(
+                              LocalizationService.translate("add_profile"),
+                              style: TextStyle(
+                                fontSize: fontSize,
+                                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                    final user = userProvider.users[index-1];
+                    return GestureDetector(
+                      onTap: isSelected ? () => _onProfileSelected(userProvider, user) : null,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: size / 2,
+                            backgroundColor: AppConfig.primaryColor,
+                            backgroundImage: (user.imageVersion != 0)
+                                ? NetworkImage(ImageService.buildImageUrl("users", user.id, version: user.imageVersion))
+                                : null,
+                            child: (user.imageVersion == 0)
+                                ? Icon(Icons.account_circle, size: size, color: AppConfig.backgroundColor)
+                                : null,
+                          ),
+                          const Padding(padding: EdgeInsets.all(4)),
+                          Text(
+                            user.name,
+                            style: TextStyle(
+                              fontSize: fontSize,
+                              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onAddProfile(UserProvider userProvider) async {
+    final result = await showDialog<Map<String, dynamic>>(
+      context: context,
+      builder: (context) => AddProfileDialog(),
+    );
+
+    if(result !=null) {
+      userProvider.addUser(
+        User(
+          id: 0,
+          name: result["name"],
+          imageVersion: (result["imagePath"] != null) ? 1 : 0
+        ), imageFile: File(result["imagePath"])
+      );
+      Navigator.of(context).popUntil((route) => route.isFirst);
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => HomeScreen()));
+    }
+  }
+
+  void _onProfileSelected(UserProvider userProvider, User user) async {
+    await userProvider.saveProfile(user);
+    Navigator.of(context).popUntil((route) => route.isFirst);
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => HomeScreen()));
+  }
+}
+
